c8c95bffb345ba64573c666623c8ce3affbb17ee
Meghan Andrewartha <megandrew1918@gmail.com>
2021-06-24 15:13:37 -0700

    updating code

---
+++ script.js
@@ -0,0 +1,13 @@
+//listen for button click
+//on button click show series of prompts
+    //length between 8 and 128 characters
+    //type of character: lowercase, uppercase, numbers, special characters
+    // make sure at least one character type is selected
+//generate a random password using a for loop
+ for (let i = 0; i < pwlength; i++) 
+    //show password in an alert or on the page
+
+var lowerCase = ["a","b","c","d","e","f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ["!", "@", "#", "$", "%", "^", "&", "*"]
